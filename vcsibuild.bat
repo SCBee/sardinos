a4dbead505c4f39bfd2715ed0ac030185fa512a8
AdamAbouelhassan <78115573+AdamAbouelhassan@users.noreply.github.com>
2024-01-03 15:33:32 -0700

    Update vcsibuild.bat

--- vcsibuild.bat
+++ vcsibuild.bat
@@ -1,8 +1,16 @@
-call vcvarsall.bat amd64 || goto :error
-call qmake.exe %1 || goto :error
+if not defined DevEnvDir (
+    call vcvarsall.bat
+)
+
+call qmake.exe %1.pro || goto :error
 call nmake.exe || goto :error
 
 echo Build Passed
+
+echo Installing to VCSi
+
+move "..\build\%1.dll" "C:\Program Files\LMCDL\vcsi\bin\plugins\vcsi"
+
 goto :end
 
 :error
