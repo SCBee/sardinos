a049f1f6d791153bd34ea46e48d8b1a0ee3eb0e9
AdamAbouelhassan <78115573+AdamAbouelhassan@users.noreply.github.com>
2024-02-03 17:10:27 -0700

    mavsdk stuff

--- custom/MissionPlanningContentCreator.cpp
+++ custom/MissionPlanningContentCreator.cpp
@@ -3,19 +3,31 @@
 #include <QLabel>
 #include <QTime>
 #include <cmath>
+#include <iostream>
+
 #include <LmCdl/ContextMenuEvent.h>
 #include <LmCdl/I_Billboard.h>
+#include <LmCdl/I_PlannedRouteCollection.h>
 #include <LmCdl/I_VcsiUserNotificationApi.h>
 #include <LmCdl/I_VectorDataDrawingApi.h>
+#include <LmCdl/StanagRoute.h>
+#include <LmCdl/StanagWaypoint.h>
 #include <LmCdl/VcsiPointOfInterestProperties.h>
 #include <LmCdl/VectorDataPolygonDrawing.h>
 #include <MathExt.cpp>
 #include <MissionDomain.h>
 #include <MissionPlanningContentCreator.h>
+#include <mavsdk/mavsdk.h>
+#include <mavsdk/plugins/action/action.h>
+#include <mavsdk/plugins/telemetry/telemetry.h>
 #include <qicon.h>
-#include <LmCdl/I_PlannedRouteCollection.h>
-#include <LmCdl/StanagRoute.h>
-#include <LmCdl/StanagWaypoint.h>
+#include <mavsdk/mavsdk.h>
+#include <mavsdk/system.h>
+#include <thread>
+
+using namespace mavsdk;
+using std::chrono::seconds;
+using std::this_thread::sleep_for;
 
 MissionPlanningContentCreator::MissionPlanningContentCreator(
     LmCdl::I_VcsiMapExtensionApi& mapApi,
@@ -70,9 +82,10 @@ void MissionPlanningContentCreator::connectToApiSignals()
             &LmCdl::I_PointOfInterestApi::pointOfInterestRemoved,
             this,
             &MissionPlanningContentCreator::updatePois);
-    }
+}
 
-void MissionPlanningContentCreator::routeAdded(){
+void MissionPlanningContentCreator::routeAdded()
+{
     notify("route added");
 }
 
@@ -95,7 +108,6 @@ void MissionPlanningContentCreator::getFlightPath()
 {
     updatePois();
 
-    
     auto route = new LmCdl::StanagRoute();
     auto waypoint = new LmCdl::StanagWaypoint();
     waypoint->location = QGeoCoordinate(51, -114);
@@ -113,9 +125,100 @@ void MissionPlanningContentCreator::getFlightPath()
     }
 }
 
+void usage(const std::string& bin_name)
+{
+    std::cerr
+        << "Usage : " << bin_name << " <connection_url>\n"
+        << "Connection URL format should be :\n"
+        << " For TCP : tcp://[server_host][:server_port]\n"
+        << " For UDP : udp://[bind_host][:bind_port]\n"
+        << " For Serial : serial:///path/to/serial/dev[:baudrate]\n"
+        << "For example, to connect to the simulator use URL: udp://:14540\n";
+}
+
 void MissionPlanningContentCreator::runMission()
 {
     notify("Starting Mission.");
+
+    auto connectStr = "";
+
+    Mavsdk mavsdk = Mavsdk();
+    mavsdk.set_configuration(Mavsdk::Configuration {Mavsdk::Configuration::UsageType::GroundStation});
+    
+    ConnectionResult connection_result = mavsdk.add_any_connection(connectStr);
+
+    if (connection_result != ConnectionResult::Success) {
+        notify("Connection failed");
+        return;
+    }
+
+    auto system = mavsdk.systems()[0];
+    if (!system) {
+        notify("Timed out waiting for system");
+        return;
+    }
+
+    // Instantiate plugins.
+    auto telemetry = Telemetry {system};
+    auto action = Action {system};
+
+    // We want to listen to the altitude of the drone at 1 Hz.
+    const auto set_rate_result = telemetry.set_rate_position(1.0);
+    if (set_rate_result != Telemetry::Result::Success) {
+        std::cerr << "Setting rate failed: " << set_rate_result << '\n';
+        return;
+    }
+
+    // Set up callback to monitor altitude while the vehicle is in flight
+    telemetry.subscribe_position(
+        [](Telemetry::Position position) {
+            std::cout << "Altitude: " << position.relative_altitude_m << " m\n";
+        });
+
+    // Check until vehicle is ready to arm
+    while (telemetry.health_all_ok() != true) {
+        std::cout << "Vehicle is getting ready to arm\n";
+        sleep_for(seconds(1));
+    }
+
+    // Arm vehicle
+    std::cout << "Arming...\n";
+    const Action::Result arm_result = action.arm();
+
+    if (arm_result != Action::Result::Success) {
+        std::cerr << "Arming failed: " << arm_result << '\n';
+        return;
+    }
+
+    // Take off
+    std::cout << "Taking off...\n";
+    const Action::Result takeoff_result = action.takeoff();
+    if (takeoff_result != Action::Result::Success) {
+        std::cerr << "Takeoff failed: " << takeoff_result << '\n';
+        return;
+    }
+
+    // Let it hover for a bit before landing again.
+    sleep_for(seconds(10));
+
+    std::cout << "Landing...\n";
+    const Action::Result land_result = action.land();
+    if (land_result != Action::Result::Success) {
+        std::cerr << "Land failed: " << land_result << '\n';
+        return;
+    }
+
+    // Check if vehicle is still in air
+    while (telemetry.in_air()) {
+        std::cout << "Vehicle is landing...\n";
+        sleep_for(seconds(1));
+    }
+    std::cout << "Landed!\n";
+
+    // We are relying on auto-disarming but let's keep watching the
+    // telemetry for a bit longer.
+    sleep_for(seconds(3));
+    std::cout << "Finished...\n";
 
     updateUIState(State::CanCancelMission);
 }
@@ -212,7 +315,6 @@ void MissionPlanningContentCreator::drawFlightPath()
             mission_.waypointConnectors())
     {
         missionApi_.addDrawingForWaypointConnector(*waypointConnector);
-
     }
 }
 
