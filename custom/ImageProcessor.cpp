1052793693d16b5b667f883a8630d4ccd29e3251
AdamAbouelhassan <78115573+AdamAbouelhassan@users.noreply.github.com>
2024-02-10 16:32:26 -0700

    comment out opencv

--- custom/ImageProcessor.cpp
+++ custom/ImageProcessor.cpp
@@ -1,14 +1,15 @@
 #include <ImageProcessor.h>
 #include <opencv2/videoio.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <iostream>
 
 void ImageProcessor::init(const std::string& uri)
 {
-    auto video = cv::VideoCapture(uri);
+    //auto video = cv::VideoCapture(uri);
 
-    cv::Mat frame;
+    //cv::Mat frame;
 
-    video >> frame;
+    //video >> frame;
 
-    cv::imwrite("test", frame);
+    //cv::imwrite("test", frame);
 }
